ee0992093c4790d149b5857ea24c23be0936ecb4
Cees van de Griend <cees.van.de.griend@xantara-it.nl>
2021-09-26 20:25:54 +0200

    Added missing initializers

--- src/main/python/trdr/candles.py
+++ src/main/python/trdr/candles.py
@@ -488,201 +488,230 @@ def update_1d(timestamp: int, market_id: int, open: float, high: float, low: flo
 
 def callback_1m(response):
     logger.debug('callback_1m() - Start')
-    market = response['market']
-    timestamp = int(response['candle'][0][0] / 1000)
-    market_id = market_cache[market]
-    open = float(response['candle'][0][1])
-    high = float(response['candle'][0][2])
-    low = float(response['candle'][0][3])
-    close = float(response['candle'][0][4])
-    volume = float(response['candle'][0][5])
+    try:
+        market = response['market']
+        timestamp = int(response['candle'][0][0] / 1000)
+        market_id = market_cache[market]
+        open = float(response['candle'][0][1])
+        high = float(response['candle'][0][2])
+        low = float(response['candle'][0][3])
+        close = float(response['candle'][0][4])
+        volume = float(response['candle'][0][5])
 
-    if market not in candle_cache:
-        candle_cache[market] = {}
-        candle_cache[market]['1m'] = {}
-        is_new = True
-    elif '1m' not in candle_cache[market]:
-        candle_cache[market]['1m'] = {}
-        is_new = True
-    elif candle_cache[market]['1m']['timestamp'] == timestamp:
-        is_new = False
-    elif candle_cache[market]['1m']['timestamp'] != timestamp:
-        is_new = True
-    else:
-        logger.error(f'callback1m(): {market} {timestamp}')
-        return
+        if market not in candle_cache:
+            candle_cache[market] = {}
+            candle_cache[market]['1m'] = {}
+            candle_cache[market]['1m']['timestamp'] = 0
+            is_new = True
+        elif '1m' not in candle_cache[market]:
+            candle_cache[market]['1m'] = {}
+            candle_cache[market]['1m']['timestamp'] = 0
+            is_new = True
+        elif 'timestamp' not in candle_cache[market]['1m']:
+            candle_cache[market]['1m']['timestamp'] = 0
+            is_new = True
+        elif candle_cache[market]['1m']['timestamp'] == timestamp:
+            is_new = False
+        elif candle_cache[market]['1m']['timestamp'] != timestamp:
+            is_new = True
+        else:
+            logger.error(f'callback1m(): {market} {timestamp}')
+            return
 
-    if is_new:
-        logger.info(
-            f"{candle_cache[market]['1m']['timestamp']} 1m " +
-            f"{market} " +
-            f"{candle_cache[market]['1m']['open']} " +
-            f"{candle_cache[market]['1m']['high']} " +
-            f"{candle_cache[market]['1m']['low']} " +
-            f"{candle_cache[market]['1m']['close']} " +
-            f"{candle_cache[market]['1m']['volume']}")
-        insert_1m(timestamp, market_id, open, high, low, close, volume)
-    else:
-        update_1m(timestamp, market_id, open, high, low, close, volume)
+        if is_new:
+            if candle_cache[market]['1m']['timestamp'] > 0:
+                logger.info(
+                    f"{candle_cache[market]['1m']['timestamp']} 1m " +
+                    f"{market} " +
+                    f"{candle_cache[market]['1m']['open']} " +
+                    f"{candle_cache[market]['1m']['high']} " +
+                    f"{candle_cache[market]['1m']['low']} " +
+                    f"{candle_cache[market]['1m']['close']} " +
+                    f"{candle_cache[market]['1m']['volume']}")
+            insert_1m(timestamp, market_id, open, high, low, close, volume)
+        else:
+            update_1m(timestamp, market_id, open, high, low, close, volume)
 
-    candle_cache[market]['1m']['timestamp'] = timestamp
-    candle_cache[market]['1m']['open'] = open
-    candle_cache[market]['1m']['high'] = high
-    candle_cache[market]['1m']['low'] = low
-    candle_cache[market]['1m']['close'] = close
-    candle_cache[market]['1m']['volume'] = volume
+        candle_cache[market]['1m']['timestamp'] = timestamp
+        candle_cache[market]['1m']['open'] = open
+        candle_cache[market]['1m']['high'] = high
+        candle_cache[market]['1m']['low'] = low
+        candle_cache[market]['1m']['close'] = close
+        candle_cache[market]['1m']['volume'] = volume
+    except Exception as e:
+        logger.error(e, exc_info=True)
 
     logger.debug('callback_1m() - Finish')
 
 
 def callback_5m(response):
     logger.debug('callback_5m() - Start')
+    try:
+        market = response['market']
+        timestamp = int(response['candle'][0][0] / 1000)
+        market_id = market_cache[market]
+        open = float(response['candle'][0][1])
+        high = float(response['candle'][0][2])
+        low = float(response['candle'][0][3])
+        close = float(response['candle'][0][4])
+        volume = float(response['candle'][0][5])
 
-    market = response['market']
-    timestamp = int(response['candle'][0][0] / 1000)
-    market_id = market_cache[market]
-    open = float(response['candle'][0][1])
-    high = float(response['candle'][0][2])
-    low = float(response['candle'][0][3])
-    close = float(response['candle'][0][4])
-    volume = float(response['candle'][0][5])
+        if market not in candle_cache:
+            candle_cache[market] = {}
+            candle_cache[market]['5m'] = {}
+            candle_cache[market]['5m']['timestamp'] = 0
+            is_new = True
+        elif '5m' not in candle_cache[market]:
+            candle_cache[market]['5m'] = {}
+            candle_cache[market]['5m']['timestamp'] = 0
+            is_new = True
+        elif 'timestamp' not in candle_cache[market]['5m']:
+            candle_cache[market]['5m']['timestamp'] = 0
+            is_new = True
+        elif candle_cache[market]['5m']['timestamp'] == timestamp:
+            is_new = False
+        elif candle_cache[market]['5m']['timestamp'] != timestamp:
+            is_new = True
+        else:
+            logger.error(f'callback_5m(): {market} {timestamp}')
+            return
 
-    if market not in candle_cache:
-        candle_cache[market] = {}
-        candle_cache[market]['5m'] = {}
-        is_new = True
-    elif '5m' not in candle_cache[market]:
-        candle_cache[market]['5m'] = {}
-        is_new = True
-    elif candle_cache[market]['5m']['timestamp'] == timestamp:
-        is_new = False
-    elif candle_cache[market]['5m']['timestamp'] != timestamp:
-        is_new = True
-    else:
-        logger.error(f'callback_5m(): {market} {timestamp}')
-        return
+        if is_new:
+            if candle_cache[market]['5m']['timestamp'] > 0:
+                logger.info(
+                    f"{candle_cache[market]['5m']['timestamp']} 5m " +
+                    f"{market} " +
+                    f"{candle_cache[market]['5m']['open']} " +
+                    f"{candle_cache[market]['5m']['high']} " +
+                    f"{candle_cache[market]['5m']['low']} " +
+                    f"{candle_cache[market]['5m']['close']} " +
+                    f"{candle_cache[market]['5m']['volume']}")
+            insert_5m(timestamp, market_id, open, high, low, close, volume)
+        else:
+            update_5m(timestamp, market_id, open, high, low, close, volume)
 
-    if is_new:
-        logger.info(
-            f"{candle_cache[market]['5m']['timestamp']} 5m " +
-            f"{market} " +
-            f"{candle_cache[market]['5m']['open']} " +
-            f"{candle_cache[market]['5m']['high']} " +
-            f"{candle_cache[market]['5m']['low']} " +
-            f"{candle_cache[market]['5m']['close']} " +
-            f"{candle_cache[market]['5m']['volume']}")
-        insert_5m(timestamp, market_id, open, high, low, close, volume)
-    else:
-        update_5m(timestamp, market_id, open, high, low, close, volume)
-
-    candle_cache[market]['5m']['timestamp'] = timestamp
-    candle_cache[market]['5m']['open'] = open
-    candle_cache[market]['5m']['high'] = high
-    candle_cache[market]['5m']['low'] = low
-    candle_cache[market]['5m']['close'] = close
-    candle_cache[market]['5m']['volume'] = volume
-
+        candle_cache[market]['5m']['timestamp'] = timestamp
+        candle_cache[market]['5m']['open'] = open
+        candle_cache[market]['5m']['high'] = high
+        candle_cache[market]['5m']['low'] = low
+        candle_cache[market]['5m']['close'] = close
+        candle_cache[market]['5m']['volume'] = volume
+    except Exception as e:
+        logger.error(e, exc_info=True)
     logger.debug('callback_5m() - Finish')
 
 
 def callback_1h(response):
     logger.debug('callback_1h() - Start')
+    try:
+        market = response['market']
+        timestamp = int(response['candle'][0][0] / 1000)
+        market_id = market_cache[market]
+        open = float(response['candle'][0][1])
+        high = float(response['candle'][0][2])
+        low = float(response['candle'][0][3])
+        close = float(response['candle'][0][4])
+        volume = float(response['candle'][0][5])
 
-    market = response['market']
-    timestamp = int(response['candle'][0][0] / 1000)
-    market_id = market_cache[market]
-    open = float(response['candle'][0][1])
-    high = float(response['candle'][0][2])
-    low = float(response['candle'][0][3])
-    close = float(response['candle'][0][4])
-    volume = float(response['candle'][0][5])
+        if market not in candle_cache:
+            candle_cache[market] = {}
+            candle_cache[market]['1h'] = {}
+            candle_cache[market]['1h']['timestamp'] = 0
+            is_new = True
+        elif '1h' not in candle_cache[market]:
+            candle_cache[market]['1h'] = {}
+            candle_cache[market]['1h']['timestamp'] = 0
+            is_new = True
+        elif 'timestamp' not in candle_cache[market]['1h']:
+            candle_cache[market]['1h']['timestamp'] = 0
+            is_new = True
+        elif candle_cache[market]['1h']['timestamp'] == timestamp:
+            is_new = False
+        elif candle_cache[market]['1h']['timestamp'] != timestamp:
+            is_new = True
+        else:
+            logger.error(f'callback_1h(): {market} {timestamp}')
+            return
 
-    if market not in candle_cache:
-        candle_cache[market] = {}
-        candle_cache[market]['1h'] = {}
-        is_new = True
-    elif '1h' not in candle_cache[market]:
-        candle_cache[market]['1h'] = {}
-        is_new = True
-    elif candle_cache[market]['1h']['timestamp'] == timestamp:
-        is_new = False
-    elif candle_cache[market]['1h']['timestamp'] != timestamp:
-        is_new = True
-    else:
-        logger.error(f'callback_1h(): {market} {timestamp}')
-        return
+        if is_new:
+            if candle_cache[market]['1h']['timestamp'] > 0:
+                logger.info(
+                    f"{candle_cache[market]['1h']['timestamp']} 1h " +
+                    f"{market} " +
+                    f"{candle_cache[market]['1h']['open']} " +
+                    f"{candle_cache[market]['1h']['high']} " +
+                    f"{candle_cache[market]['1h']['low']} " +
+                    f"{candle_cache[market]['1h']['close']} " +
+                    f"{candle_cache[market]['1h']['volume']}")
+            insert_1h(timestamp, market_id, open, high, low, close, volume)
+        else:
+            update_1h(timestamp, market_id, open, high, low, close, volume)
 
-    if is_new:
-        logger.info(
-            f"{candle_cache[market]['1h']['timestamp']} 1h " +
-            f"{market} " +
-            f"{candle_cache[market]['1h']['open']} " +
-            f"{candle_cache[market]['1h']['high']} " +
-            f"{candle_cache[market]['1h']['low']} " +
-            f"{candle_cache[market]['1h']['close']} " +
-            f"{candle_cache[market]['1h']['volume']}")
-
-        insert_1h(timestamp, market_id, open, high, low, close, volume)
-    else:
-        update_1h(timestamp, market_id, open, high, low, close, volume)
-
-    candle_cache[market]['1h']['timestamp'] = timestamp
-    candle_cache[market]['1h']['open'] = open
-    candle_cache[market]['1h']['high'] = high
-    candle_cache[market]['1h']['low'] = low
-    candle_cache[market]['1h']['close'] = close
-    candle_cache[market]['1h']['volume'] = volume
-
+        candle_cache[market]['1h']['timestamp'] = timestamp
+        candle_cache[market]['1h']['open'] = open
+        candle_cache[market]['1h']['high'] = high
+        candle_cache[market]['1h']['low'] = low
+        candle_cache[market]['1h']['close'] = close
+        candle_cache[market]['1h']['volume'] = volume
+    except Exception as e:
+        logger.error(e, exc_info=True)
     logger.debug('callback_1h() - Finish')
 
 
 def callback_1d(response):
     logger.debug('callback_1d() - Start')
+    try:
+        market = response['market']
+        timestamp = int(response['candle'][0][0] / 1000)
+        market_id = market_cache[market]
+        open = float(response['candle'][0][1])
+        high = float(response['candle'][0][2])
+        low = float(response['candle'][0][3])
+        close = float(response['candle'][0][4])
+        volume = float(response['candle'][0][5])
 
-    market = response['market']
-    timestamp = int(response['candle'][0][0] / 1000)
-    market_id = market_cache[market]
-    open = float(response['candle'][0][1])
-    high = float(response['candle'][0][2])
-    low = float(response['candle'][0][3])
-    close = float(response['candle'][0][4])
-    volume = float(response['candle'][0][5])
+        if market not in candle_cache:
+            candle_cache[market] = {}
+            candle_cache[market]['1d'] = {}
+            candle_cache[market]['1d']['timestamp'] = 0
+            is_new = True
+        elif '1d' not in candle_cache[market]:
+            candle_cache[market]['1d'] = {}
+            candle_cache[market]['1d']['timestamp'] = 0
+            is_new = True
+        elif 'timestamp' not in candle_cache[market]['1d']:
+            candle_cache[market]['1d']['timestamp'] = 0
+            is_new = True
+        elif candle_cache[market]['1d']['timestamp'] == timestamp:
+            is_new = False
+        elif candle_cache[market]['1d']['timestamp'] != timestamp:
+            is_new = True
+        else:
+            logger.error(f'callback_1d(): {market} {timestamp}')
+            return
 
-    if market not in candle_cache:
-        candle_cache[market] = {}
-        candle_cache[market]['1d'] = {}
-        is_new = True
-    elif '1d' not in candle_cache[market]:
-        candle_cache[market]['1d'] = {}
-        is_new = True
-    elif candle_cache[market]['1d']['timestamp'] == timestamp:
-        is_new = False
-    elif candle_cache[market]['1d']['timestamp'] != timestamp:
-        is_new = True
-    else:
-        logger.error(f'callback_1d(): {market} {timestamp}')
-        return
+        if is_new:
+            if candle_cache[market]['1d']['timestamp'] > 0:
+                logger.info(
+                    f"{candle_cache[market]['1d']['timestamp']} 1d " +
+                    f"{market} " +
+                    f"{candle_cache[market]['1d']['open']} " +
+                    f"{candle_cache[market]['1d']['high']} " +
+                    f"{candle_cache[market]['1d']['low']} " +
+                    f"{candle_cache[market]['1d']['close']} " +
+                    f"{candle_cache[market]['1d']['volume']}")
+            insert_1d(timestamp, market_id, open, high, low, close, volume)
+        else:
+            update_1d(timestamp, market_id, open, high, low, close, volume)
 
-    if is_new:
-        logger.info(
-            f"{candle_cache[market]['1d']['timestamp']} 1d " +
-            f"{market} " +
-            f"{candle_cache[market]['1d']['open']} " +
-            f"{candle_cache[market]['1d']['high']} " +
-            f"{candle_cache[market]['1d']['low']} " +
-            f"{candle_cache[market]['1d']['close']} " +
-            f"{candle_cache[market]['1d']['volume']}")
-        insert_1d(timestamp, market_id, open, high, low, close, volume)
-    else:
-        update_1d(timestamp, market_id, open, high, low, close, volume)
-
-    candle_cache[market]['1d']['timestamp'] = timestamp
-    candle_cache[market]['1d']['open'] = open
-    candle_cache[market]['1d']['high'] = high
-    candle_cache[market]['1d']['low'] = low
-    candle_cache[market]['1d']['close'] = close
-    candle_cache[market]['1d']['volume'] = volume
-
+        candle_cache[market]['1d']['timestamp'] = timestamp
+        candle_cache[market]['1d']['open'] = open
+        candle_cache[market]['1d']['high'] = high
+        candle_cache[market]['1d']['low'] = low
+        candle_cache[market]['1d']['close'] = close
+        candle_cache[market]['1d']['volume'] = volume
+    except Exception as e:
+        logger.error(e, exc_info=True)
     logger.debug('callback_1d() - Finish')
 
 
